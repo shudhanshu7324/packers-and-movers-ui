0dbbbdb834adfb13d364027ab1ff2a9573b7879c
Shudhanshu Shree <83982549+shudhanshu7324@users.noreply.github.com>
2024-09-03 17:23:13 +0530

    fontsize fixed

--- script.js
+++ script.js
@@ -1,27 +1,29 @@
 document.querySelectorAll(".more-than-four").forEach((section) => {
-  const moreButton = section.querySelector(".view-more");
-  const moreItems = section.nextElementSibling.querySelector(".more-items");
-
-  moreButton.addEventListener("click", () => {
-    if (moreItems.style.display === "flex") {
-      moreItems.style.display = "none";
-      moreButton.textContent = "View More";
-    } else {
-      moreItems.style.display = "flex";
-      moreButton.textContent = "View Less";
-    }
+    const moreButton = section.querySelector(".view-more");
+    const moreItems = section.nextElementSibling.querySelector(".more-items");
+  
+    moreButton.addEventListener("click", () => {
+      if (moreItems.style.display === "flex") {
+        moreItems.style.display = "none";
+        moreButton.textContent = "View More";
+      } else {
+        moreItems.style.display = "flex";
+        moreButton.textContent = "View Less";
+        
+        section.scrollIntoView({ behavior: "smooth", block: "start" });
+      }
+    });
   });
-});
-
-// handling image click url
-const figureTags = document.querySelectorAll("figure");
-
-for (let i = 0; i < figureTags.length; i++) {
-  figureTags[i].addEventListener("click", () => {
-    const figureTag = figureTags[i];
-    const figCaption = figureTag.querySelector(":nth-child(2)").textContent;
-    const uri = `https://customerapp.movingsolutions.in/dashboard/enquiry.php?qType=packer&Specific=${figCaption}`;
-    const encoded = encodeURI(uri);
-    console.log(encoded);
+  
+  // Handling image click URL
+  const figureTags = document.querySelectorAll("figure");
+  
+  figureTags.forEach(figureTag => {
+    figureTag.addEventListener("click", () => {
+      const figCaption = figureTag.querySelector("figcaption").textContent;
+      const uri = `https://customerapp.movingsolutions.in/dashboard/enquiry.php?qType=packer&Specific=${figCaption}`;
+      const encoded = encodeURI(uri);
+      window.location.href = encoded;
+    });
   });
-}
+  
